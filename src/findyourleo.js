b82a4df02ad8d5fccd52e83a3d070f758238b548
Bugnone <bugnone.michael@gmail.com>
2020-12-21 01:14:48 +0100

    base server

--- src/findyourleo.js
+++ src/findyourleo.js
@@ -3,11 +3,35 @@ var app = express();
 var path = require('path');
 
 app.get('/file/:name', function (req, res) {
-  var file = req.params.name
+  var file = path.join( __dirname, '../data/' + req.params.name)
 
-  // res.sendFile('/data/' + file);
-  res.sendFile(path.join( __dirname, '../data/' + file));
-})
+  res.sendFile(file, function(err) {
+    if(err) {
+      console.log(new Date().toString());
+      console.log(err);
+      res.status(404).send('Sorry! You can\'t see that.');
+    }
+    else {
+      console.log(new Date().toString());
+      console.log(file);
+    }
+  });
+});
+
+app.get('/data/:name', function(req, res) {
+  let levels = [];
+  levels.push({
+    id: 1,
+    amount: 10,
+    path: '1.jpg',
+  });
+  levels.push({
+    id: 2,
+    amount: 50,
+    path: '2.jpg',
+  });
+  res.send({levels: levels});
+});
 
 app.listen(4000, function (err) {
   if (err) console.log(err);
